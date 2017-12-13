24e620e4587aad744815814fec88995bdb8ce291
praveendeshmane <deshmane.praveen@gmail.com>
2017-12-13 23:45:31 +0530

    Initial commit

---
+++ app/src/main/java/in/co/praveendeshmane/firebase/User.java
@@ -0,0 +1,24 @@
+package in.co.praveendeshmane.firebase;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * Created by Admin on 08-12-2017.
+ */
+
+@IgnoreExtraProperties
+public class User {
+
+    public String name;
+    public String email;
+
+    // Default constructor required for calls to
+    // DataSnapshot.getValue(User.class)
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
